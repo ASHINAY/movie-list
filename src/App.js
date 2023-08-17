ef24199c70df2dde8d679cfa19064bb25e438f88
Nayak <anuja.nayak@capgemini.com>
2023-08-17 19:06:06 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
 import ListPage from './pages/ListPage/ListPage.jsx';
 import DetailsPage from './pages/DetailsPage/DetailsPage.jsx';
 
@@ -6,12 +6,12 @@ import DetailsPage from './pages/DetailsPage/DetailsPage.jsx';
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route exact path='/' element={<ListPage />} />
           <Route exact path='/DetailsPage' element={<DetailsPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
